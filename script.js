40a2c4ad80cb08f7b61e29f9e11753a4c7a195c7
JotaJotaM1 <johnjames_89@hotmail.com>
2024-03-19 16:02:33 -0500

    se ajustan cosas adicionales despues de la entrega

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ var fechaCuentaRegresiva = new Date("Apr 3, 2024 00:00:00").getTime();
 // Actualiza la cuenta regresiva cada segundo
 var x = setInterval(function () {
   // Obtiene la fecha y hora de hoy
-var ahora = new Date().getTime();
+  var ahora = new Date().getTime();
 
   // Encuentra la distancia entre ahora y la fecha de cuenta regresiva
-var distancia = fechaCuentaRegresiva - ahora;
+  var distancia = fechaCuentaRegresiva - ahora;
 
   // Calcula tiempo para días, horas, minutos y segundos
   var dias = Math.floor(distancia / (1000 * 60 * 60 * 24));
-var horas = Math.floor(
+  var horas = Math.floor(
     (distancia % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-);
+  );
   var minutos = Math.floor((distancia % (1000 * 60 * 60)) / (1000 * 60));
   var segundos = Math.floor((distancia % (1000 * 60)) / 1000);
 
@@ -32,66 +32,70 @@ var horas = Math.floor(
   }
 }, 1000);
 
-
-
 (function () {
-    "use strict";
-    window.addEventListener(
-      "load",
-      function () {
-        // Inicializa EmailJS con tu API key pública
-        emailjs.init("ooKBOkLJ2xA-zMv_4"); // <-- Asegúrate de reemplazar "tu_api_key_publica" con tu clave pública real
-  
-        var forms = document.getElementsByClassName("needs-validation");
-        Array.prototype.filter.call(forms, function (form) {
-          form.addEventListener(
-            "submit",
-            function (event) {
-              if (!form.checkValidity()) {
-                event.preventDefault();
-                event.stopPropagation();
-              }
-              form.classList.add("was-validated");
-            },
-            false
-          );
-        });
-      },
-      false
-    );
-  })();
+  "use strict";
+  window.addEventListener(
+    "load",
+    function () {
+      // Inicializa EmailJS con tu API key pública
+      emailjs.init("ooKBOkLJ2xA-zMv_4"); // <-- Asegúrate de reemplazar "tu_api_key_publica" con tu clave pública real
 
-document.getElementById("miFormulario").addEventListener("submit", function (event) {
+      var forms = document.getElementsByClassName("needs-validation");
+      Array.prototype.filter.call(forms, function (form) {
+        form.addEventListener(
+          "submit",
+          function (event) {
+            if (!form.checkValidity()) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add("was-validated");
+          },
+          false
+        );
+      });
+    },
+    false
+  );
+})();
+
+document
+  .getElementById("miFormulario")
+  .addEventListener("submit", function (event) {
     event.preventDefault(); // Previene el envío predeterminado
 
     if (this.checkValidity()) {
-        // Intenta enviar el formulario con EmailJS
-        emailjs.sendForm("service_8q0ecmb", "template_fsmo7r9", this)
-            .then(function () {
-                Swal.fire({
-                    title: "¡Enviado!",
-                    text: "Tu mensaje ha sido enviado con éxito.",
-                    icon: "success",
-                }).then(function (result) {
-                    if (result.isConfirmed) {
-                        window.location.reload();
-                    }
-                });
-            }, function (error) {
-                Swal.fire({
-                    title: "Error",
-                    text: "Tu mensaje no pudo ser enviado. " + error.text,
-                    icon: "error",
-                });
-            });
-    } else {
-        // Muestra un mensaje de error si el formulario no es válido
-        this.classList.add("was-validated");
-        Swal.fire({
+      // Intenta enviar el formulario con EmailJS
+      emailjs.sendForm("service_8q0ecmb", "template_fsmo7r9", this).then(
+        function () {
+          Swal.fire({
+            title: "¡Enviado!",
+            text: "¡Pronto nos pondremos en contacto contigo!",
+            icon: "success",
+            customClass: {
+              formulario: "alertaEnvio",
+            },
+          }).then(function (result) {
+            if (result.isConfirmed) {
+              window.location.reload();
+            }
+          });
+        },
+        function (error) {
+          Swal.fire({
             title: "Error",
-            text: "Por favor, completa el formulario correctamente antes de enviar.",
+            text: "Tu mensaje no pudo ser enviado. " + error.text,
             icon: "error",
-        });
+          });
+        }
+      );
+    } else {
+      // Muestra un mensaje de error si el formulario no es válido
+      this.classList.add("was-validated");
+      Swal.fire({
+        title: "Error",
+        text: "Por favor, completa el formulario correctamente antes de enviar.",
+        icon: "error",
+      });
     }
-});
-
+  });
